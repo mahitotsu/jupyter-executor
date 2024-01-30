879e7315fdf9d8dc74449e455c9b5297ffc60c21
akring <akio@mail.com>
2024-01-31 00:36:08 +0900

    modified

--- assets/app.py
+++ assets/app.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from nbconvert.nbconvertapp import NbConvertApp
 from nbconvert.exporters import HTMLExporter
@@ -7,9 +8,18 @@ from nbconvert.writers import FilesWriter
 def lambda_handler (event, context):
 
     name = event["rawPath"].lstrip("/")
+    if name == "favicon.ico":
+        return {
+            "statusCode": 200,
+            "body": "",
+        }
+
+    executor = ExecutePreprocessor()
 
     exporter = HTMLExporter()
-    exporter.register_preprocessor(ExecutePreprocessor(), True)
+    exporter.register_preprocessor(executor, True)
+    exporter.mathjax_url = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/latest.js?config=TeX-AMS_CHTML-full,Safe"
+    exporter.require_js_url = "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js"
 
     writer = FilesWriter()
     writer.build_directory = "/tmp"
@@ -18,6 +28,9 @@ def lambda_handler (event, context):
     app = NbConvertApp()
     app.exporter = exporter
     app.writer = writer
+
+    os.environ["var1"] = str(123)
+    os.environ["var2"] = str(456)
     app.convert_single_notebook(f"./notebooks/{name}.ipynb")
 
     return {
