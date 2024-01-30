01a94b575bbedfd1b85e7457c36ae882702341b9
akring <akio@mail.com>
2024-01-30 18:04:11 +0900

    modified

--- assets/app.py
+++ assets/app.py
@@ -6,22 +6,24 @@ from nbconvert.writers import FilesWriter
 
 def lambda_handler (event, context):
 
+    name = event["rawPath"].lstrip("/")
+
     exporter = HTMLExporter()
     exporter.register_preprocessor(ExecutePreprocessor(), True)
 
     writer = FilesWriter()
     writer.build_directory = "/tmp"
-    writer.relpath = "hello.html"
+    writer.relpath = f"{name}.html"
 
     app = NbConvertApp()
     app.exporter = exporter
     app.writer = writer
-    app.convert_single_notebook("./hello.ipynb")
+    app.convert_single_notebook(f"./notebooks/{name}.ipynb")
 
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "text/html"
         },
-        "body": Path("/tmp/hello.html").read_text()
+        "body": Path(f"{writer.build_directory}/{writer.relpath}").read_text()
     }
